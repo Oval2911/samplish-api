1007363591184bb335b0c09ae89c82cc23cc2d57
fnfs <farhannfs5121@gmail.com>
2022-07-30 22:58:33 +0700

    brand -> list

--- app/Controllers/Brand.php
+++ app/Controllers/Brand.php
@@ -25,8 +25,14 @@ class Brand extends ResourceController
         $filters = array(
             "limit" => $this->request->getGet("limit"),
         );
+        $fields = array(
+            "brand.idbrand",
+            "brand.name",
+            "brand_category.name as category",
+            "brand.variant",
+        );
 
-        $data = $this->brand->datatable(array('*'), $filters);
+        $data = $this->brand->datatable($fields, $filters);
 
         return $this->respond(
             tempResponse(
@@ -40,91 +46,6 @@ class Brand extends ResourceController
                 )
             )
         );
-    }
- 
-    // get single product
-    public function show($id = null)
-    {
-        $model = new SamplersModel();
-        $data = $model->get_sampler(array(), array());
-        if($data){
-            return $this->respond($data);
-        }else{
-            return $this->failNotFound('No Data Found ');
-        }
-    }
-
-    // create a product
-    public function create()
-    {
-        $model = new ProductModel();
-        $data = [
-            'product_name' => $this->request->getPost('product_name'),
-            'product_price' => $this->request->getPost('product_price')
-        ];
-        $data = json_decode(file_get_contents("php://input"));
-        //$data = $this->request->getPost();
-        $model->insert($data);
-        $response = [
-            'status'   => 201,
-            'error'    => null,
-            'messages' => [
-                'success' => 'Data Saved'
-            ]
-        ];
-         
-        return $this->respondCreated($data, 201);
-    }
- 
-    // update product
-    public function update($id = null)
-    {
-        $model = new ProductModel();
-        $json = $this->request->getJSON();
-        if($json){
-            $data = [
-                'product_name' => $json->product_name,
-                'product_price' => $json->product_price
-            ];
-        }else{
-            $input = $this->request->getRawInput();
-            $data = [
-                'product_name' => $input['product_name'],
-                'product_price' => $input['product_price']
-            ];
-        }
-        // Insert to Database
-        $model->update($id, $data);
-        $response = [
-            'status'   => 200,
-            'error'    => null,
-            'messages' => [
-                'success' => 'Data Updated'
-            ]
-        ];
-        return $this->respond($response);
-    }
- 
-    // delete product
-    public function delete($id = null)
-    {
-        $model = new ProductModel();
-        $data = $model->find($id);
-        if($data){
-            $model->delete($id);
-            $response = [
-                'status'   => 200,
-                'error'    => null,
-                'messages' => [
-                    'success' => 'Data Deleted'
-                ]
-            ];
-             
-            return $this->respondDeleted($response);
-        }else{
-            return $this->failNotFound('No Data Found with id '.$id);
-        }
-         
     }
  
 }
--- app/Models/BrandModel.php
+++ app/Models/BrandModel.php
@@ -18,45 +18,11 @@ class BrandModel extends Model
 
     }
 
-    public function get($columns = array('*'), $filter = array())
-    {
-
-        $builder = $this->dbCanvazer->table('brand');
-        $builder->select($columns);
-
-        if (isset($filter['filter'])) {
-            $builder->where($filter['filter']);
-        }
-        if (isset($filter['filternot'])) {
-            $builder->where($filter['filternot']);
-        }
-        if (isset($filter['filterLike'])) {
-            $builder->like($filter['filterLike']);
-        }
-
-        if (isset($filter['limit'])) {
-            $builder->limit($filter['limit']['n_item'], $filter['limit']['page'] * $filter['limit']['n_item']);
-        }
-
-        if (isset($filter['sort'])) {
-            foreach ($filter['sort'] as $key => $value) {
-                $builder->orderBy($key, $value);
-            }
-        }
-
-        $query = $builder->get();
-        if ($query->getResultArray() > 0) {
-            $result = $query->getResultArray();
-            return $result;
-        } else {
-            return false;
-        }
-    }
-
     public function datatable($columns = array('*'), $filter = array())
     {
         $data = $this->dbCanvazer->table('brand');
-        $data->select($columns);
+        $data->select($columns)
+            ->join("brand_category","brand_category.idcategorybrand = brand.idcategorybrand");
 
         $total = $this->dbCanvazer->table('brand');
         $total->select("COUNT(idbrand) as amount");
@@ -76,11 +42,11 @@ class BrandModel extends Model
 
         $data->limit($filter['limit']['n_item'], $filter['limit']['page'] * $filter['limit']['n_item']);
 
-        // if (isset($filter['sort'])) {
-        //     foreach ($filter['sort'] as $key => $value) {
-        //         $data->orderBy($key, $value);
-        //     }
-        // }
+        if (isset($filter['sort'])) {
+            foreach ($filter['sort'] as $key => $value) {
+                $data->orderBy($key, $value);
+            }
+        }
 
         $total = $total->get()->getResultArray()[0]['amount'];
 
@@ -90,51 +56,4 @@ class BrandModel extends Model
             "total_pages" => round($total / $filter['limit']['n_item']),
         ];
     }
-
-// [END_GET_FUNCTIONS]
-
-// [START_UPDATE_FUNCTIONS]
-
-    public function update_seller($data, $filter)
-    {
-        $builder->set($data);
-        $builder->where($filter);
-        $builder->update('t_user_seller');
-        // echo $builder->last_query();
-
-        $n_affected_rows = $builder->affected_rows();
-        return $n_affected_rows;
-    }
-
-// [END_UPDATE_FUNCTIONS]
-
-// [START_INSERT_FUNCTIONS]
-
-    public function insert_seller($data)
-    {
-        $builder->set('ts_registration', 'NOW()', false);
-        $builder->insert('t_user_seller', $data);
-        // echo $builder->last_query();
-        return $builder->insert_id();
-    }
-
-
-// [END_INSERT_FUNCTIONS]
-
-// [START_DELETE_FUNCTIONS]
-
-    public function delete_seller($filter)
-    {
-        $builder->where($filter);
-        $builder->delete('t_user_seller');
-        // echo $builder->last_query();
-
-        $n_affected_rows = $builder->affected_rows();
-        return $n_affected_rows;
-    }
-
-
-// [END_DELETE_FUNCTIONS]
-
-
 }
