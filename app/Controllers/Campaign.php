18a6a317408eb2d7bd78da3d1585e8585fb061f4
fnfs <farhannfs5121@gmail.com>
2022-08-13 23:11:10 +0700

    ~

--- app/Controllers/Campaign.php
+++ app/Controllers/Campaign.php
@@ -67,12 +67,6 @@ class Campaign extends ResourceController
                 'key' => ["label"=>"Key", "rules"=>"required",],
                 'u' => ["label"=>"User", "rules"=>"required",],
                 'token' => ["label"=>"Access Token", "rules"=>"required",],
-                'service' => ["label"=>"Pick Up Service", "rules"=>"required",],
-                'service_address' => ["label"=>"Address", "rules"=>"required",],
-                'service_due_date' => ["label"=>"Date", "rules"=>"required",],
-                'contact_name' => ["label"=>"Contact Name", "rules"=>"required",],
-                'contact_number' => ["label"=>"Contact Number", "rules"=>"required",],
-                'receipt_payment' => ["label"=>"Proof of Payment", "rules"=>"uploaded[receipt_payment]",],
             ],
             "amend_brands" => [
                 'key' => ["label"=>"Key", "rules"=>"required",],
